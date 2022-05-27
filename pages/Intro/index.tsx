112d0a4511126c95b93bc27271a1b63ba6f6e4de
Thiago Saraiva <thiagosaraiva@Thiagos-MacBook-Air.local>
2022-05-27 13:00:09 -0300

    Adjust in package.json and tsconfig

--- pages/Intro/index.tsx
+++ pages/Intro/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import Navbar from "../../src/components/navbar";
+import Navbar from "../../src/components/Navbar";
 import Image from "../../src/components/image";
 import Button from "../../src/components/button";
 import Modal from "../../src/components/modal";
