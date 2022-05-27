ef570014561c3115ea2b22aeb9ae8c5722a22e81
Thiago Saraiva <thiagosaraiva@Thiagos-MacBook-Air.local>
2022-05-27 13:11:00 -0300

    Adjusts

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import Header from "./Header";
-import Input from "../Input/";
+import Input from "../Input/index";
 import styles from "./styles.module.scss";
 import {
   addSubscribeLeads,
